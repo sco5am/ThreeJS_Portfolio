20f9af7684169df04796b1edf2b8baab1e349baa
Samuel Oaks <samuelclayoaks@yahoo.com>
2023-06-17 18:08:56 -0400

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import live from './live.png'
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import mysql from './tech/mysql.png'
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  live,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  mysql,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  carrent,
+  jobit,
+  tripguide,
+};
